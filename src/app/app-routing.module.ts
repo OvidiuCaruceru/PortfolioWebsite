3b1f2d8631d876fb51aeb79b966487ff51699861
Ovidiu Caruceru <caruceruovidiu@yahoo.com>
2023-03-30 23:57:17 +0300

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './components/contact/contact.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Intro', component: IntroComponent},
+  {path: 'Skills', component: SkillsComponent},
+  {path: 'Projects', component: ProjectsComponent},
+  {path: 'Contact', component: ContactComponent},
+  {path: '', redirectTo: '/Intro', pathMatch: 'full'},
+  {path: '**', redirectTo: '/Intro', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
