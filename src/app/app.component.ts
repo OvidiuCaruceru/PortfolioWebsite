1eebc5b92e850ddbd9ad14e25bc8261ebd465d65
Ovidiu Caruceru <caruceruovidiu@yahoo.com>
2023-05-22 00:52:31 +0300

    Add background text functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Observable, Subscription, debounceTime, fromEvent } from 'rxjs';
 import { ComponentNameService } from './services/component-name.service';
 
 @Component({
@@ -6,22 +7,71 @@ import { ComponentNameService } from './services/component-name.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   
-  title = 'Căruceru Ovidiu';
+  title = 'Căruceru Ovidiu'; // website title
+  activeComponentName = ''; // background text displaying active component name
 
-  activeComponentName = '';
+  // subscriptions
+  activeComponentNameSubscription!: Subscription;
+  resizeSubscription!: Subscription;
+
+  // access to template variables
+  @ViewChild('mainContainer') mainContainer!: ElementRef;
+  @ViewChild('backgroundTitle') backgroundTitle!: ElementRef;
 
   constructor(
     private componentNameService: ComponentNameService,
-    private cdRef: ChangeDetectorRef // Angular is detecting a change in the value of componentName that has occurred after the view has been checked, and is throwing an error without this additional change detection
+    private renderer: Renderer2 // modify the DOM in order to set the font size dynamically
     ) { }
 
   ngOnInit() {
-    this.componentNameService.activeComponentName.subscribe(componentName => {
+    // subscription to update activeComponentName with the name of the selected component
+    this.activeComponentNameSubscription = this.componentNameService.activeComponentName.subscribe(componentName => {
       this.activeComponentName = componentName;
-      this.cdRef.detectChanges();
     });
+
+    // subscription to react to window resizing
+    this.resizeSubscription = this.createResizeObservable().subscribe();
   }
 
+  ngAfterViewChecked() {
+    this.fitBackgroundTitle(); // Call fitBackgroundTitle() when activeComponentName changes
+  }
+  
+  ngOnDestroy() {
+    this.activeComponentNameSubscription.unsubscribe();
+    this.resizeSubscription.unsubscribe();
+  }
+
+  // Check the width of the word and resize the font if necessary to proprely fit the container
+  private fitBackgroundTitle() {
+    const container = this.mainContainer.nativeElement;
+    const containerHeight = container.offsetHeight;
+    const containerWidth = container.offsetWidth;
+    const maxBackgroundTitleSize = containerHeight * 0.9;
+
+    const backgroundTitleContainer = this.backgroundTitle.nativeElement;
+
+    // Set the font size to match container's width
+    const initialFontSize = containerWidth * 1.2;
+
+    // Render the element with the initial font size
+    this.renderer.setStyle(backgroundTitleContainer, 'fontSize', `${initialFontSize}px`);
+
+    // Check if the word's width exceeds the container's height
+    const titleWidth = backgroundTitleContainer.offsetHeight;
+
+    if (titleWidth > maxBackgroundTitleSize) {
+      const updatedFontSize = initialFontSize * (maxBackgroundTitleSize / titleWidth);
+      // Re-render the element with updated font size
+      this.renderer.setStyle(backgroundTitleContainer, 'fontSize', `${updatedFontSize}px`);
+    }
+  }
+
+  // react to window resizing
+  private createResizeObservable(): Observable<any> {
+    return fromEvent(window, 'resize');
+  }
 }
+
