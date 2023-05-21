7c2ff7394306ce2d066cf1bd31ec909ef89acada
Ovidiu Caruceru <caruceruovidiu@yahoo.com>
2023-05-22 00:49:51 +0300

    Add interaction functionality

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ComponentNameService } from 'src/app/services/component-name.service';
+import { Project } from '../../models/project.model'
+import { projects } from 'src/app/data/projects-data';
 
 @Component({
   selector: 'app-projects',
@@ -9,10 +11,17 @@ import { ComponentNameService } from 'src/app/services/component-name.service';
 export class ProjectsComponent implements OnInit {
 
   componentName = 'Projects';
+  showDetails = true;
+  projects: Project[] = projects;
 
   constructor(private componentNameService: ComponentNameService) { }
 
   ngOnInit() {
     this.componentNameService.updateComponentName(this.componentName);
   }
+
+  toggleDetails () {
+    this.showDetails = !this.showDetails;
+  }
+
 }
