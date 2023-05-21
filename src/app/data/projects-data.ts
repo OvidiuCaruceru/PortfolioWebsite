d007d7a4ef389937b6438672c74043745d66ed4f
Ovidiu Caruceru <caruceruovidiu@yahoo.com>
2023-05-22 00:47:29 +0300

    Add projects data

---
+++ src/app/data/projects-data.ts
@@ -0,0 +1,15 @@
+import { Project } from "../models/project.model";
+
+export const projects: Project[] = [
+    {
+        title: 'Portfolio website',
+        website: 'https://example.com',
+        github: 'https://github.com/OvidiuCaruceru/PortfolioWebsite',
+        figma: 'https://www.figma.com/file/6rNudfS9QZ4pGuqoixmK2J/Portfolio-website?type=design&t=qMmRmfK2cvudZbPg-0',
+        description: [
+          'The website you are viewing right now.',
+          'Designed in Figma and built using Angular.',
+          'Demonstrating my ability to transpose a design to functional code.',
+        ],
+    }
+]
