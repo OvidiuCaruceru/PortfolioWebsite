b1b9bb0144df5888d755ee7fa9784a0fabc320cf
Ovidiu Caruceru <caruceruovidiu@yahoo.com>
2023-05-23 03:54:41 +0300

    Update data

--- src/app/data/projects-data.ts
+++ src/app/data/projects-data.ts
@@ -1,15 +1,56 @@
 import { Project } from "../models/project.model";
 
 export const projects: Project[] = [
-    {
-        title: 'Portfolio website',
-        website: 'https://example.com',
-        github: 'https://github.com/OvidiuCaruceru/PortfolioWebsite',
-        figma: 'https://www.figma.com/file/6rNudfS9QZ4pGuqoixmK2J/Portfolio-website?type=design&t=qMmRmfK2cvudZbPg-0',
-        description: [
-          'The website you are viewing right now.',
-          'Designed in Figma and built using Angular.',
-          'Demonstrating my ability to transpose a design to functional code.',
-        ],
-    }
+  {
+    title: 'Portfolio website',
+    website: 'https://example.com',
+    github: 'https://github.com/OvidiuCaruceru/PortfolioWebsite',
+    figma: 'https://www.figma.com/file/6rNudfS9QZ4pGuqoixmK2J/Portfolio-website?type=design&t=qMmRmfK2cvudZbPg-0',
+    description: [
+      {
+        text: 'The website you are viewing right now.',
+        hasSpecialStyling: false
+      },
+      {
+        text: 'Designed in Figma and built using Angular.',
+        hasSpecialStyling: false
+      },
+      {
+        text: 'Demonstrating my ability to transpose a design to functional code.',
+        hasSpecialStyling: false
+      },
+      {
+        text: 'Highlights: Angular app built from scratch, routing, data binding, service, observable.',
+        hasSpecialStyling: false
+      },
+    ],
+    isActive: false,
+    isFinished: true
+  },
+  {
+    title: 'Fullstack website with ChatGPT',
+    website: 'pending',
+    github: 'pending',
+    figma: 'pending',
+    description: [
+      {
+        text: 'Coming soon',
+        hasSpecialStyling: true
+      },
+      {
+        text: 'Website to display my learning process using ChatGPT.',
+        hasSpecialStyling: false
+      },
+      {
+        text: 'Designed in Figma and built using Angular and ASP.NET.',
+        hasSpecialStyling: false
+      },
+      {
+        text: 'Highlights: CRUD operations, working with data from server, authentication.',
+        hasSpecialStyling: false
+      },
+    ],
+    isActive: false,
+    isFinished: false
+  },
 ]
