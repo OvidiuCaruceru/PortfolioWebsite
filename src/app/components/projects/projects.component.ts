0d93686a23dbb15a342af7b5978d508e2c7fb6e1
Ovidiu Caruceru <caruceruovidiu@yahoo.com>
2023-05-26 03:24:26 +0300

    Update logic to work on both mobile and desktop

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { ComponentDataService } from 'src/app/services/component-data.service';
 import { Project } from '../../models/project.model'
 import { projects } from 'src/app/data/projects-data';
@@ -12,15 +12,22 @@ export class ProjectsComponent implements OnInit {
 
   componentImageUrl = '../assets/images/projects.svg';
   projects: Project[] = projects;
+  isBigScreen!: boolean;
 
   constructor(private componentDataService: ComponentDataService) { }
 
   ngOnInit() {
     this.componentDataService.updateComponentImageUrl(this.componentImageUrl);
+    this.isBigScreen = window.innerWidth >= 550;
   }
 
   toggleDetails(project: Project) {
     project.isActive = !project.isActive;
   }
 
+  // listen for window resizing in order to know if mobile or desktop layout is used
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.isBigScreen = window.innerWidth >= 550;
+  }
 }
